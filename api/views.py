3f4ad486a5ec31d5dc58dbf5345d56ce81dc2b69
Rushil Kumar <rkumar@seatgeek.com>
2019-03-22 13:13:01 -0400

    Added pagination for API

--- api/views.py
+++ api/views.py
@@ -12,6 +12,8 @@ query_curated_reviews = 'SELECT name, comment, gender FROM curated_reviews'
 
 insert_curated_reviews = ('INSERT INTO curated_reviews (review_id, comment, name, gender) VALUES (%s, %s, %s, %s)')
 
+PER_PAGE = 20
+
 @blueprint_http.route('/api/external-reviews')
 def external_reviews():
     response = {
@@ -19,11 +21,14 @@ def external_reviews():
         'total' : 0
     }
     query = query_reviews
-    query_params = {}
+    query_params = { 'per_page' : PER_PAGE}
     keyword = request.args.get('keyword', '')
     query_params['keyword'] = '%' + keyword + '%'
     max_length = request.args.get('max_length', None)
     min_length = request.args.get('min_length', None)
+    page = int(request.args.get('page', 1))
+    offset = (page - 1) * PER_PAGE
+    query_params['offset'] = offset
     cursor = cnx.cursor()
     if max_length is not None:
         query_params['max_length'] = max_length
@@ -31,6 +36,7 @@ def external_reviews():
     if min_length is not None:
         query_params['min_length'] = min_length
         query = query + ' AND CHAR_LENGTH(r.comment) > %(min_length)s'
+    query = query + ' LIMIT %(per_page)s OFFSET %(offset)s'
     cursor.execute(query, query_params)
     for (review_id, name, comment, linked_id) in cursor:
         review = {
@@ -42,6 +48,7 @@ def external_reviews():
         response['reviews'].append(review)
         response['total'] = response['total'] + 1
     cursor.close()
+    response['page'] = page
     return jsonify(response)
 
 @blueprint_http.route('/api/curated-reviews/<review_id>', methods=['POST'])
@@ -63,7 +70,13 @@ def get_curated_reviews():
         'reviews' : [],
         'total' : 0
     }
-    cursor.execute(query_curated_reviews)
+    query = query_curated_reviews
+    query_params = {'per_page' : PER_PAGE}
+    page = int(request.args.get('page', 1))
+    offset = (page - 1) * PER_PAGE
+    query_params['offset'] = offset
+    query = query + ' LIMIT %(per_page)s OFFSET %(offset)s'
+    cursor.execute(query, query_params)
     for (name, comment, gender) in cursor:
         review = {
             'name' : name,
@@ -73,4 +86,5 @@ def get_curated_reviews():
         response['reviews'].append(review)
         response['total'] = response['total'] + 1
     cursor.close()
+    response['page'] = page
     return jsonify(response)
