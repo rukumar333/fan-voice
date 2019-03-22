78de50d681a19cc4a00518de24e2fea8f9982b2c
Rushil Kumar <rkumar@seatgeek.com>
2019-03-22 12:23:15 -0400

    Added limit for character length to curated and external reviews.

--- api/views.py
+++ api/views.py
@@ -6,7 +6,9 @@ blueprint_http = Blueprint('blueprint_http', __name__)
 
 cnx = mysql.connector.connect(user=config.DB_USER, password=config.DB_PASS, host=config.DB_HOST, database=config.DB_NAME)
 query_reviews = ('SELECT r.id, r.name, r.comment, c.review_id as linked_id FROM reviews r LEFT JOIN curated_reviews c on c.review_id=r.id WHERE r.comment LIKE %s')
+query_reviews_limit = ('SELECT r.id, r.name, r.comment, c.review_id as linked_id FROM reviews r LEFT JOIN curated_reviews c on c.review_id=r.id WHERE r.comment LIKE %s AND CHAR_LENGTH(r.comment) < %s')
 query_curated_reviews = ('SELECT name, comment, gender FROM curated_reviews')
+query_curated_reviews_limit = ('SELECT name, comment, gender FROM curated_reviews WHERE CHAR_LENGTH(comment) < %s')
 insert_curated_reviews = ('INSERT INTO curated_reviews (review_id, comment, name, gender) VALUES (%s, %s, %s, %s)')
 
 @blueprint_http.route('/api/external-reviews')
@@ -16,8 +18,12 @@ def external_reviews():
         'total' : 0
     }
     keyword = request.args.get('keyword', '')
+    char_limit = request.args.get('char_limit', None)
     cursor = cnx.cursor()
-    cursor.execute(query_reviews, ('%' + keyword + '%',))
+    if char_limit is None:
+        cursor.execute(query_reviews, ('%' + keyword + '%',))
+    else:
+        cursor.execute(query_reviews_limit, ('%' + keyword + '%', int(char_limit)))
     for (review_id, name, comment, linked_id) in cursor:
         review = {
             'id' : review_id,
@@ -49,7 +55,11 @@ def get_curated_reviews():
         'reviews' : [],
         'total' : 0
     }
-    cursor.execute(query_curated_reviews)
+    char_limit = request.args.get('char_limit', None)
+    if char_limit is None:
+        cursor.execute(query_curated_reviews)
+    else:
+        cursor.execute(query_curated_reviews_limit, (int(char_limit),))
     for (name, comment, gender) in cursor:
         review = {
             'name' : name,
