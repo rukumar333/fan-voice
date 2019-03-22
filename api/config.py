bfdf18e7bf702d4f6508ca9547b776dad3c8e7be
Rushil Kumar <rkumar@seatgeek.com>
2019-03-22 10:39:41 -0400

    Updated external reviews endpoint to read from the database. Updated config to read db info from env.

--- api/config.py
+++ api/config.py
@@ -1,3 +1,7 @@
 import os
 
 PORT = os.getenv('PORT', 3030)
+DB_USER = os.getenv('DB_USER', 'fv')
+DB_PASS = os.getenv('DB_PASS', '')
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1')
+DB_NAME = os.getenv('DB_NAME', 'fanvoice')
--- api/views.py
+++ api/views.py
@@ -1,14 +1,26 @@
 from flask import Blueprint, jsonify
+import config
+import mysql.connector
 
 blueprint_http = Blueprint('blueprint_http', __name__)
 
+cnx = mysql.connector.connect(user=config.DB_USER, password=config.DB_PASS, host=config.DB_HOST, database=config.DB_NAME)
+query_reviews = ('SELECT name, comment FROM reviews')
+
 @blueprint_http.route('/api/external-reviews')
 def external_reviews():
-    return jsonify({
-        'reviews': [
-            {'name': 'George Daole-Wellman', 'text': 'SeatGeek is great'},
-            {'name': 'Rushil Kumar', 'text': 'It\s the best'},
-            {'name': 'Locke Anderson', 'text': 'Awesome'}
-        ],
-        'total': 3
-    })
+    response = {
+        'reviews' : [],
+        'total' : 0
+    }
+    cursor = cnx.cursor()
+    cursor.execute(query_reviews)
+    for (name, comment) in cursor:
+        review = {
+            'name' : name,
+            'text' : comment
+        }
+        response['reviews'].append(review)
+        response['total'] = response['total'] + 1
+    cursor.close()
+    return jsonify(response)
