b8ff92f3bad3d182ec2a73dc4d2cbb41abae004a
Rushil Kumar <rkumar@seatgeek.com>
2019-03-22 13:03:48 -0400

    Fixed length queries for external-reviews. Removed for curated.

--- api/views.py
+++ api/views.py
@@ -5,10 +5,11 @@ import mysql.connector
 blueprint_http = Blueprint('blueprint_http', __name__)
 
 cnx = mysql.connector.connect(user=config.DB_USER, password=config.DB_PASS, host=config.DB_HOST, database=config.DB_NAME)
-query_reviews = ('SELECT r.id, r.name, r.comment, c.review_id as linked_id FROM reviews r LEFT JOIN curated_reviews c on c.review_id=r.id WHERE r.comment LIKE %s')
-query_reviews_limit = ('SELECT r.id, r.name, r.comment, c.review_id as linked_id FROM reviews r LEFT JOIN curated_reviews c on c.review_id=r.id WHERE r.comment LIKE %s AND CHAR_LENGTH(r.comment) < %s')
-query_curated_reviews = ('SELECT name, comment, gender FROM curated_reviews')
-query_curated_reviews_limit = ('SELECT name, comment, gender FROM curated_reviews WHERE CHAR_LENGTH(comment) < %s')
+
+query_reviews = 'SELECT r.id, r.name, r.comment, c.review_id as linked_id FROM reviews r LEFT JOIN curated_reviews c on c.review_id=r.id WHERE r.comment LIKE %(keyword)s'
+
+query_curated_reviews = 'SELECT name, comment, gender FROM curated_reviews'
+
 insert_curated_reviews = ('INSERT INTO curated_reviews (review_id, comment, name, gender) VALUES (%s, %s, %s, %s)')
 
 @blueprint_http.route('/api/external-reviews')
@@ -17,13 +18,20 @@ def external_reviews():
         'reviews' : [],
         'total' : 0
     }
+    query = query_reviews
+    query_params = {}
     keyword = request.args.get('keyword', '')
-    char_limit = request.args.get('char_limit', None)
+    query_params['keyword'] = '%' + keyword + '%'
+    max_length = request.args.get('max_length', None)
+    min_length = request.args.get('min_length', None)
     cursor = cnx.cursor()
-    if char_limit is None:
-        cursor.execute(query_reviews, ('%' + keyword + '%',))
-    else:
-        cursor.execute(query_reviews_limit, ('%' + keyword + '%', int(char_limit)))
+    if max_length is not None:
+        query_params['max_length'] = max_length
+        query = query + ' AND CHAR_LENGTH(r.comment) < %(max_length)s'
+    if min_length is not None:
+        query_params['min_length'] = min_length
+        query = query + ' AND CHAR_LENGTH(r.comment) > %(min_length)s'
+    cursor.execute(query, query_params)
     for (review_id, name, comment, linked_id) in cursor:
         review = {
             'id' : review_id,
@@ -55,11 +63,7 @@ def get_curated_reviews():
         'reviews' : [],
         'total' : 0
     }
-    char_limit = request.args.get('char_limit', None)
-    if char_limit is None:
-        cursor.execute(query_curated_reviews)
-    else:
-        cursor.execute(query_curated_reviews_limit, (int(char_limit),))
+    cursor.execute(query_curated_reviews)
     for (name, comment, gender) in cursor:
         review = {
             'name' : name,
