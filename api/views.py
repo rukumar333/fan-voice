4923146bb65fd425859ac41d8225612100600dac
Rushil Kumar <rkumar@seatgeek.com>
2019-03-22 11:33:58 -0400

    Added POST for curated reviews to API.

--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ blueprint_http = Blueprint('blueprint_http', __name__)
 cnx = mysql.connector.connect(user=config.DB_USER, password=config.DB_PASS, host=config.DB_HOST, database=config.DB_NAME)
 query_reviews = ('SELECT name, comment FROM reviews')
 query_curated_reviews = ('SELECT name, comment, gender FROM curated_reviews')
+insert_curated_reviews = ('INSERT INTO curated_reviews (review_id, comment, name, gender) VALUES (%s, %s, %s, %s)')
 
 @blueprint_http.route('/api/external-reviews')
 def external_reviews():
@@ -19,31 +20,40 @@ def external_reviews():
     for (name, comment) in cursor:
         review = {
             'name' : name,
-            'text' : comment
+            'quote' : comment
         }
         response['reviews'].append(review)
         response['total'] = response['total'] + 1
     cursor.close()
     return jsonify(response)
 
-@blueprint_http.route('/api/curated-reviews', methods=['GET', 'POST'])
-def curated_reviews():
+@blueprint_http.route('/api/curated-reviews/<review_id>', methods=['POST'])
+def add_curated_reviews(review_id):
     cursor = cnx.cursor()
-    if request.method == 'POST':
-        print('Adding curated review')
-    else:
-        response = {
-            'reviews' : [],
-            'total' : 0
+    body = request.get_json()
+    cursor.execute(insert_curated_reviews, (review_id, body['quote'], body['name'], body['gender']))
+    cnx.commit()
+    cursor.close()
+    response = {
+        'status': 'Successfully added curated review'
+    }
+    return jsonify(response)
+
+@blueprint_http.route('/api/curated-reviews')
+def get_curated_reviews():
+    cursor = cnx.cursor()
+    response = {
+        'reviews' : [],
+        'total' : 0
+    }
+    cursor.execute(query_curated_reviews)
+    for (name, comment, gender) in cursor:
+        review = {
+            'name' : name,
+            'quote' : comment,
+            'gender' : gender
         }
-        cursor.execute(query_curated_reviews)
-        for (name, comment, gender) in cursor:
-            review = {
-                'name' : name,
-                'quote' : comment,
-                'gender' : gender
-            }
-            response['reviews'].append(review)
-            response['total'] = response['total'] + 1
-        cursor.close()
-        return jsonify(response)
+        response['reviews'].append(review)
+        response['total'] = response['total'] + 1
+    cursor.close()
+    return jsonify(response)
